0d1ad69b1023b5662752d4f84a643b761d4a90ee
Kevin <kevinmao.xy@gmail.com>
2021-01-28 13:54:38 -0500

    add associations in Enrollment model

--- skeleton/app/models/enrollment.rb
+++ skeleton/app/models/enrollment.rb
@@ -1,2 +1,11 @@
 class Enrollment < ApplicationRecord
+    has_many :course,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
+
+    has_many :user,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
 end
